bc0fa8b4a8fd050d69ad4f8b13ebc6b7b3594bd0
ryderwishart <ryderwishart@gmail.com>
2026-04-18 11:10:12 -0600

    feat(living-memory): wire route and flag-gated sidebar entry

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ProjectWorkspace } from "@/components/ProjectWorkspace"
 import { ProjectSettings } from "@/components/ProjectSettings"
 import { DebugView } from "@/components/DebugView"
 import { RulesPage } from "@/components/RulesPage"
+import { LivingMemoryPage } from "@/components/LivingMemoryPage"
 import { CommentsPage } from "@/components/CommentsPage"
 import { SnapshotsPage } from "@/components/SnapshotsPage"
 import { JoinPage } from "@/components/JoinPage"
@@ -40,6 +41,7 @@ function AppRoutes() {
       <Route path="/project/:id/settings" element={<ProjectSettings />} />
       <Route path="/project/:id/settings/debug" element={<DebugView />} />
       <Route path="/project/:id/rules" element={<RulesPage />} />
+      <Route path="/project/:id/memory" element={<LivingMemoryPage />} />
       <Route path="/project/:id/comments" element={<CommentsPage />} />
       <Route path="/project/:id/comments/debug" element={<DebugView />} />
       <Route path="/project/:id/snapshots" element={<SnapshotsPage />} />
